04f3fb0cf9ce1202ceef23b83ade2b5ef6e13e20
Ubuntu <ubuntu@ip-172-31-94-135.ec2.internal>
2020-01-30 22:53:56 +0000

    create skills list view

--- routes/skills.js
+++ routes/skills.js
@@ -17,14 +17,12 @@ router.post('/', async (req, res) => {
 
 router.get('/:letter', async (req, res) => {
   const { letter } = req.params;
-
+  
   if (letter.length > 1) res.send('Only one letter');
 
   try {
-    const skills = Skill.find({
-      title: { $regex: '^' + letter },
-      $options: 'i'
-    });
+    const skills = await Skill.find({
+      "title": { $regex: '^' + letter , $options: 'i'}});
     res.render('skillsList', { skills });
   } catch (err) {
     res.send(err);
