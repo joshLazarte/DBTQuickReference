b98c9f80821356a0ec82d685cedb24b6fe1af560
Josh <joshualazarte@yahoo.com>
2020-02-06 18:02:40 -0600

    fix mobile display issues

--- public/js/src/eventListeners.js
+++ public/js/src/eventListeners.js
@@ -1,20 +1,21 @@
 import ui from './uiElements';
-import {toggleModal, writeModal, showNextSkill, showPrevSkill} from './modal';
+import { toggleModal, writeModal, showNextSkill, showPrevSkill } from './modal';
 
 const loadEventListners = () => {
-    ui.showSkillsBtn.forEach(btn => {
-        const {skillslist} = btn.dataset;
-        btn.addEventListener('click', () => {
-            toggleModal();
-            writeModal(skillslist);
-        });
+  ui.showSkillsBtn.forEach(btn => {
+    const { skillslist } = btn.dataset;
+    btn.addEventListener('click', e => {
+      e.preventDefault();
+      toggleModal();
+      writeModal(skillslist);
     });
-    
-    ui.backdrop.addEventListener('click', () => toggleModal());
-    
-    ui.modalPrevArrow.addEventListener('click', () => showPrevSkill());
-    
-    ui.modalNextArrow.addEventListener('click', () => showNextSkill());
+  });
+
+  ui.backdrop.addEventListener('click', () => toggleModal());
+
+  ui.modalPrevArrow.addEventListener('click', () => showPrevSkill());
+
+  ui.modalNextArrow.addEventListener('click', () => showNextSkill());
 };
 
 export default loadEventListners;
--- public/js/src/modal.js
+++ public/js/src/modal.js
@@ -3,46 +3,46 @@ import masterSkillsList from './skills';
 import _gs from './globalState';
 
 const toggleModal = () => {
-    ui.modal.classList.toggle('modal--open');
-    ui.backdrop.classList.toggle('backdrop--open');
-    ui.modalTitle.textContent = '';
-    ui.modalContent.innerHTML = '';
-    _gs.currentSkillsList = '';
-    _gs.currentSkillIndex = 0;
+  ui.modal.classList.toggle('modal--open');
+  ui.backdrop.classList.toggle('backdrop--open');
+  ui.modalTitle.textContent = '';
+  ui.modalContent.innerHTML = '';
+  _gs.currentSkillsList = '';
+  _gs.currentSkillIndex = 0;
 };
 
 const writeModal = skills => {
-    _gs.currentSkillsList = skills;
-    _gs.currentSkillIndex = 0;
-    const list = masterSkillsList[skills];
-    ui.modalTitle.textContent = list[_gs.currentSkillIndex].title;
-    ui.modalContent.innerHTML = list[_gs.currentSkillIndex].content;
+  _gs.currentSkillsList = skills;
+  _gs.currentSkillIndex = 0;
+  const list = masterSkillsList[skills];
+  ui.modalTitle.textContent = list[_gs.currentSkillIndex].title;
+  ui.modalContent.innerHTML = list[_gs.currentSkillIndex].content;
 };
 
 const showNextSkill = () => {
-    const list = masterSkillsList[_gs.currentSkillsList];
-    
-    if(_gs.currentSkillIndex === list.length - 1){
-        _gs.currentSkillIndex = 0;
-    } else {
-        _gs.currentSkillIndex += 1;
-    }
-    
-    ui.modalTitle.textContent = list[_gs.currentSkillIndex].title;
-    ui.modalContent.innerHTML = list[_gs.currentSkillIndex].content;
+  const list = masterSkillsList[_gs.currentSkillsList];
+
+  if (_gs.currentSkillIndex === list.length - 1) {
+    _gs.currentSkillIndex = 0;
+  } else {
+    _gs.currentSkillIndex += 1;
+  }
+
+  ui.modalTitle.textContent = list[_gs.currentSkillIndex].title;
+  ui.modalContent.innerHTML = list[_gs.currentSkillIndex].content;
 };
 
 const showPrevSkill = () => {
-    const list = masterSkillsList[_gs.currentSkillsList];
-    
-     if(_gs.currentSkillIndex === 0){
-        _gs.currentSkillIndex = list.length - 1;
-    } else {
-        _gs.currentSkillIndex -= 1;
-    }
-    
-    ui.modalTitle.textContent = list[_gs.currentSkillIndex].title;
-    ui.modalContent.innerHTML = list[_gs.currentSkillIndex].content;
+  const list = masterSkillsList[_gs.currentSkillsList];
+
+  if (_gs.currentSkillIndex === 0) {
+    _gs.currentSkillIndex = list.length - 1;
+  } else {
+    _gs.currentSkillIndex -= 1;
+  }
+
+  ui.modalTitle.textContent = list[_gs.currentSkillIndex].title;
+  ui.modalContent.innerHTML = list[_gs.currentSkillIndex].content;
 };
 
-export {toggleModal, writeModal, showNextSkill, showPrevSkill};
+export { toggleModal, writeModal, showNextSkill, showPrevSkill };
